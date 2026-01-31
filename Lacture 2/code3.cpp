5a33522b005a24b3f9730715c1e5dbf68c3ddb8a
Kanak Yadav <yadavkanak109@gmail.com>
2026-01-31 23:17:07 +0530

    leacture 2 complete

---
+++ Lacture 2/code3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ int a, b;
+ cout << "Enter A" << '\n';
+ cin >> a ;
+
+ cout << "Enter B" << '\n' ;
+ cin >> b ;
+
+ int sum = a+b;
+
+
+ int y = 10 ;
+ int z = ++y;
+cout << "Sum of A and B is :  " << sum << "\n" << y << "\n" << z << "\n";
+
+    return 0 ;
+}
