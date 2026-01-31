8538df09f306203e76cdfc5a12b022db87a679f7
Kanak Yadav <yadavkanak109@gmail.com>
2026-01-31 23:24:40 +0530

    lacture 2 homework done

---
+++ Lacture 2/homework.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+
+using namespace std;
+ 
+
+int main() {
+ 
+    int a ,b ; 
+
+    cout << "Enter A : " << "\n" ;
+    cin >>  a  ;
+
+    cout << "Enter B :" << "\n" ;
+    cin >> b  ;
+
+    cout << "Sum is :" << a+b << "\n" ;
+    cout << "Multiply  is :" << a*b << "\n" ;
+    cout << "Subtract  is :" << a-b << "\n" ;
+    cout << "Devide is :" << a/b << "\n" ;
+    cout << "Modulo is :" << a%b << "\n" ;
+
+
+
+    return 0 ;
+}
