3eb434eb60d753802d038e4f30485dbf75bdaa0b
Kanak Yadav <yadavkanak109@gmail.com>
2026-02-01 13:00:32 +0530

    lecture 3 code 6

---
+++ Lacture 3/code6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+ 
+    int n ;
+
+    cout << "Enter Iteration Number : ";
+    cin >> n ;
+
+    for(int i = 0 ; i<=n ;i++){
+        cout << i << " ";
+    }
+
+    cout << "\n";
+
+
+
+    return 0 ;
+}
