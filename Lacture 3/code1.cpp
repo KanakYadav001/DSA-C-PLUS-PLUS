b2e3dcd703be910ef7f211f63ef5ba4506f809fe
Kanak Yadav <yadavkanak109@gmail.com>
2026-02-01 11:58:02 +0530

    lacture 3 code 1 and 2

---
+++ Lacture 3/code1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  
+    int a  = -10 ;
+
+    if(a>=0){
+        cout << "positive Number" << "\n";
+    }else {
+        cout << "Nagitive Number " << "\n";
+    }
+ 
+
+    return 0 ;
+}
---
+++ Lacture 3/code2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(){
+
+  int a = 10;
+
+  if(a%2==0){
+    cout << "This is Even" << "\n";
+  }
+
+
+
+    return 0 ;
+}
