9a441a945dcb5af9610c45feeb2aec5795977bf6
Kanak Yadav <yadavkanak109@gmail.com>
2026-02-02 23:18:12 +0530

    lecture 4  question 14 15

---
+++ Lacture 4/code14.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+  int n ;
+
+  cout << "Enter N Line Number : " ;
+  cin >> n ;
+
+
+  for(int i=0 ;i<n ;i++){
+
+    for(int j=0 ; j<i ;j++){
+        cout << " " ;
+    }
+
+    for(int k=0 ;k<n-i;k++){
+        cout << i+1  ;
+    }
+
+    cout << "\n";
+  }
+
+
+
+
+
+    return 0;
+}
