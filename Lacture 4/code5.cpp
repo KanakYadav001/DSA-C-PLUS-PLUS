a06f3085f7aefbc22380d5ffae888cbf68bf0a11
Kanak Yadav <yadavkanak109@gmail.com>
2026-02-02 17:22:45 +0530

    lacture 4 code 5

---
+++ Lacture 4/code5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using  namespace std;
+
+
+int main(){
+
+  int n ;
+   char ch = 'A';
+
+  cout << "Enter N Lines : ";
+  cin >> n ;
+
+
+  for(int i=0 ; i<n ;i++) {
+    for(int j=0 ;j<n ;j++){
+        cout << ch << " " ;
+        ch +=1 ;
+
+    }
+
+    cout << "\n";
+  }
+
+
+
+    return 0;
+}
