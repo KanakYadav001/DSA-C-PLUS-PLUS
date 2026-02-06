89384b173c37e4f6d6d19e77089a2c1dde16bbf3
Kanak Yadav <yadavkanak109@gmail.com>
2026-02-06 10:12:47 +0530

    lecture 8 code 2

---
+++ Lacture 8/code2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int size =5 ;
+ int arr[size] = {10,20,30,40,50};
+
+ for(int i=0;i<size ;i++) {
+    cout << arr[i] << " " ;
+ }
+
+
+
+    return 0;
+}
