f02cc8c28f1ffe3056e551bf8e5eb76bc5771e5f
Kanak Yadav <yadavkanak109@gmail.com>
2026-02-06 10:09:12 +0530

    lecture 8 code 1

---
+++ Lacture 8/code1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(){
+   
+    long arr[10] = {20,30,40,50,60,70,80,90,100,110};    
+    
+    cout << arr[1] << "\n";
+
+
+
+
+    return 0;
+}
