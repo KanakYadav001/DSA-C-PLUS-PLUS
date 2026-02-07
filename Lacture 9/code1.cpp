9657e4d4dcc69e929851574c486c663eae4363d0
Kanak Yadav <yadavkanak109@gmail.com>
2026-02-07 21:15:18 +0530

    lecture 9 code 1

---
+++ Lacture 9/code1.cpp
@@ -0,0 +1,18 @@
+//vectors
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+
+    vector <int> vec ={2,5,6,7,8,9};
+
+    for(int val : vec){
+        cout << val << " ";
+    }
+
+
+    return 0 ;
+}
